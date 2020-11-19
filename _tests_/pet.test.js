04bf19c524a65cdcb71ec39b2541c01ed6f50245
Jasmin Bateman <jasminbateman@gmail.com>
2020-11-19 08:55:09 +0000

    growUp function + test created

--- _tests_/pet.test.js
+++ _tests_/pet.test.js
@@ -4,11 +4,27 @@ describe("constructor", () => {
     it("returns an object", () => {
         expect(new Pet("Fido")).toBeInstanceOf(Object);
     });
+
+    it("has an initial age of 0", () => {
+        const pet = new Pet("Fido");
+
+        expect(pet.age).toEqual(0);
+    });
 });
 
-describe("Fido exists", () => {
+describe("growUp", () => {
+    it("increments the age by 1", () => {
+        const pet = new Pet("Fido");
+
+        pet.growUp();
+
+        expect(pet.age).toEqual(1);
+    });
+});
+
+describe("pet exists", () => {
     it("sets name of pet", () => {
         const pet = new Pet("Fido");
         expect(pet.name).toEqual("Fido");
-    })
+    });
 });
